fb25b28824c7b48a1fb002782c83e858c9ef41c4
Roy <58099388+RoyMk@users.noreply.github.com>
2025-01-19 23:56:18 -0500

    new JS

--- js.js
+++ js.js
@@ -1,50 +1,100 @@
 document.addEventListener("DOMContentLoaded", () => {
+    class CartStorage {
+        static KEY = "CartQtyCounter";
+        static BADGE_CLASS = "show-shopping-cart-badge";
 
-    class CartManager {
-
-        constructor() {
+        static getQuantity() {
+            return localStorage.getItem(this.KEY);
         }
 
-        counter = 0
-
-        addToCart(badge, badgeCounter) {
-            this.counter++
-            badge.classList.add('show-shopping-cart-badge');
-            badgeCounter.textContent = this.counter.toString();
+        static setQuantity(value) {
+            localStorage.setItem(this.KEY, value.toString());
         }
-
-
     }
 
-    // Handles UI interaction
-    class UiManager {
-        #cartBadge
-        #cartBadgeCounter
+    class CartUI {
+        constructor() {
+            this.badge = document.querySelector(".cart-badge");
+            this.counter = document.querySelector(".badge-counter");
+        }
+
+        updateDisplay() {
+            if (!this.badge || !this.counter) {
+                console.warn("Cart UI elements not found");
+                return;
+            }
+
+            const quantity = CartStorage.getQuantity();
+
+            if (quantity !== null) {
+                this.badge.classList.add(CartStorage.BADGE_CLASS);
+                this.counter.textContent = quantity;
+            } else {
+                this.badge.classList.remove(CartStorage.BADGE_CLASS);
+                this.counter.textContent = "";
+            }
+        }
+    }
+
+    class CartManager {
+        constructor(uiManager) {
+            this.uiManager = uiManager;
+            this.quantity = parseInt(CartStorage.getQuantity() || "0");
+        }
+
+        addItem() {
+            this.quantity++;
+            CartStorage.setQuantity(this.quantity);
+            this.updateUI();
+        }
+
+        updateUI() {
+            const badge = this.uiManager.getCartBadge();
+            const counter = this.uiManager.getCartCounter();
+
+            if (badge && counter) {
+                badge.classList.add(CartStorage.BADGE_CLASS);
+                counter.textContent = this.quantity.toString();
+            }
+        }
+    }
+
+    class UIManager {
+        #cartBadge;
+        #cartCounter;
         #addToCartButton;
 
-        constructor({cart, cartManager, card}) {
+        constructor({cart, card}) {
             this.cart = cart;
-            this.cartManager = cartManager;
             this.card = card;
 
-            this.#cartBadge = this.cart.querySelector(".cart-badge")
-            this.#cartBadgeCounter = this.cart.querySelector(".badge-counter")
-            this.#addToCartButton = this.card.querySelector("#addToCartButton")
-            this.#bindEvents()
+            this.#cartBadge = this.cart.querySelector(".cart-badge");
+            this.#cartCounter = this.cart.querySelector(".badge-counter");
+            this.#addToCartButton = this.card.querySelector("#addToCartButton");
+        }
+
+        initializeCartManager(cartManager) {
+            this.cartManager = cartManager;
+            this.#bindEvents();
         }
 
         #bindEvents() {
             this.#addToCartButton.addEventListener("click", () => {
-                this.cartManager.addToCart(this.#cartBadge, this.#cartBadgeCounter)
-            })
+                this.cartManager.addItem();
+            });
+        }
+
+        getCartBadge() {
+            return this.#cartBadge;
+        }
+
+        getCartCounter() {
+            return this.#cartCounter;
         }
     }
 
-    class Formatter {
-        constructor() {
-        }
-
-        static formatCurrency(amount, currencyCode = 'USD', locale = 'en-US', decimalPlaces = 2) {
+    class CurrencyFormatter {
+        static format(amount, currencyCode = 'USD', locale = 'en-US', decimalPlaces = 2) {
             const formatter = new Intl.NumberFormat(locale, {
                 style: 'currency',
                 currency: currencyCode,
@@ -54,17 +104,20 @@ document.addEventListener("DOMContentLoaded", () => {
             return formatter.format(amount);
         }
 
-        static removeCurrencyFormatting(str) {
+        static unformat(str) {
             return str.replace(/[$,]/g, '');
         }
     }
 
-    let cardA = document.querySelector('#CardA');
-    new UiManager({
+    // Initialization
+    const cartUI = new CartUI();
+    cartUI.updateDisplay();
+
+    const uiManager = new UIManager({
         cart: document.querySelector('.shopping-cart'),
-        cartManager: new CartManager(),
         card: document.querySelector('#cardA'),
     });
 
+    const cartManager = new CartManager(uiManager);
+    uiManager.initializeCartManager(cartManager);
 });
-
